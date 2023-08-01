6ff32e8b4d1730ffdfe3b3d2c2a15d099e433130
Matt Healy <bigolehealz@Matts-MacBook-Pro.local>
2023-08-01 14:58:15 -0400

    removing top-level packages.json

--- flask-server/server.py
+++ flask-server/server.py
@@ -8,7 +8,7 @@ CORS(app)
 
 @app.route("/members")
 def members():
-    return {"members": ["member1", "member2", "member3"]}
+    return {"members": ["member1", "member2", "member3", "member4"]}
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- rerun_docker.sh
+++ rerun_docker.sh
@@ -1,4 +1,28 @@
 #! /bin/sh
+
+if lsof -Pi :5001 -sTCP:LISTEN -t >/dev/null ; then
+    kill -9 $(lsof -t -i:5001)
+fi
+
+if lsof -Pi :19006 -sTCP:LISTEN -t >/dev/null ; then
+    kill -9 $(lsof -t -i:19006)
+fi
+
+#!/bin/bash
+
+if ! docker info >/dev/null 2>&1; then
+    echo "Starting Docker..."
+    # Path might be different depending on the location of Docker app
+    open /Applications/Docker.app
+    # Wait until Docker daemon is running and has completed initialisation
+    while (! docker info >/dev/null 2>&1 ); do
+        # Docker takes a few seconds to initialize
+        echo "Waiting for Docker to launch..."
+        sleep 1
+    done
+fi
+
+
 docker container prune -f
 docker image prune -f
 
