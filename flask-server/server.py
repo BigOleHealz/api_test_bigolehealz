8ef0c204ed8e51e6665f95417528c98b1a5aea9f
Matt Healy <bigolehealz@Matts-MacBook-Pro.local>
2023-08-01 14:41:45 -0400

    initial commit

---
+++ flask-server/server.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/members")
+def members():
+    return {"members": ["member1", "member2", "member3"]}
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
